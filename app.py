c9b67fcce56d43d582ecd78ff134d2f64e8f8743
Oyaki122 <oc.yaki122@gmail.com>
2023-07-07 17:44:31 +0900

    環境構築

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, g, redirect, url_for, session, flash
-import uuid
 import hashlib
 import sqlite3
-import os, datetime
-from dataclasses import dataclass
+import os
 from datetime import timedelta
+import datetime
+from flask import Flask, render_template, request, g, redirect, url_for, session, flash
 
 DATABASE = "database.db"
 app = Flask(__name__, static_folder='./static')
 app.config['SECRET_KEY'] = '1234'
+
 
 @app.before_request
 def before_request():
@@ -17,9 +17,12 @@ def before_request():
     app.permanent_session_lifetime = timedelta(minutes=15)
     session.modified = True
 
+
 """
 データベース接続
 """
+
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -27,15 +30,19 @@ def get_db():
         db.row_factory = sqlite3.Row
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+
 """
 ビュー
 """
+
+
 @app.route('/')
 def home():
     # セッションにuser_idがない場合、未ログインなのでusernameを渡さずにHTMLをレンダリング
@@ -44,8 +51,10 @@ def home():
     # ユーザ情報がある場合、データベースに接続しuser_idからユーザ名を検索して渡す
     conn = get_db()
     cur = conn.cursor()
-    user = cur.execute('SELECT * FROM LoginUser WHERE username = ?', (session['user_id'],)).fetchone()
+    user = cur.execute('SELECT * FROM LoginUser WHERE username = ?',
+                       (session['user_id'], )).fetchone()
     return render_template('home.html', username=user['username'])
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -61,17 +70,20 @@ def login():
         h = hashlib.md5(password.encode())
         conn = get_db()
         cur = conn.cursor()
-        user = cur.execute('SELECT * FROM LoginUser WHERE username = ? AND password = ?', (username, h.hexdigest())).fetchone()
+        user = cur.execute(
+            'SELECT * FROM LoginUser WHERE username = ? AND password = ?',
+            (username, h.hexdigest())).fetchone()
         if user is None:
             # ユーザ認証されなかった場合、エラーメッセージをレンダリングする。メッセージはこのように引数で渡すこともできるし、else以下のようにflashを使用することもできる。
             error = 'Invalid username or password'
         else:
             # ユーザ認証された場合、セッションに記録する
-            session['user_id'] = username 
+            session['user_id'] = username
             # flashメッセージを設定する
             flash("Logged in")
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -92,18 +104,22 @@ def signup():
         if password != confirm:
             flash('Passwords do not match')
             request_valid = False
-        if cur.execute('SELECT * FROM LoginUser WHERE username = ?',(username,)).fetchone():
+        if cur.execute('SELECT * FROM LoginUser WHERE username = ?',
+                       (username, )).fetchone():
             # ユーザーがすでに存在している場合はエラーを表示する
             flash('This username is already taken')
             request_valid = False
         if request_valid:
             # 新しいユーザーを作成する
             h = hashlib.md5(password.encode())
-            cur.execute('INSERT INTO LoginUser (username, password) VALUES (?, ?)', (username, h.hexdigest()))
+            cur.execute(
+                'INSERT INTO LoginUser (username, password) VALUES (?, ?)',
+                (username, h.hexdigest()))
             conn.commit()
             # ログインページにリダイレクトする
             return redirect(url_for('login'))
     return render_template('signup.html')
+
 
 @app.route('/logout')
 def logout():
@@ -120,13 +136,21 @@ def employee():
     cur = conn.cursor()
     # POSTは検索をするとリクエストされる
     if request.method == "POST":
-        elist = cur.execute("SELECT e.*, m.name as manager_name FROM employee e, employee m WHERE m.id = e.manager AND e.name LIKE ? ORDER BY e.id", ('%'+request.form["name"]+'%', )).fetchall()
-        return render_template("employee.html", counts=len(elist), elist=elist, query_name=request.form["name"])
+        elist = cur.execute(
+            "SELECT e.*, m.name as manager_name FROM employee e, employee m WHERE m.id = e.manager AND e.name LIKE ? ORDER BY e.id",
+            ('%' + request.form["name"] + '%', )).fetchall()
+        return render_template("employee.html",
+                               counts=len(elist),
+                               elist=elist,
+                               query_name=request.form["name"])
     else:
         # GETの場合全件のデータベースルックアップをする。返ってくるテーブルの属性名で属性にアクセスするので、SQLで属性名を適切に設定する
-        elist = cur.execute("SELECT e.*, m.name as manager_name FROM employee e, employee m WHERE m.id = e.manager ORDER BY e.id").fetchall() ## XXX: managerがいないと表示されない。managerがいない人も表示させるにはouter joinする。
+        elist = cur.execute(
+            "SELECT e.*, m.name as manager_name FROM employee e, employee m WHERE m.id = e.manager ORDER BY e.id"
+        ).fetchall(
+        )  ## XXX: managerがいないと表示されない。managerがいない人も表示させるにはouter joinする。
         return render_template("employee.html", counts=len(elist), elist=elist)
-    
+
 
 @app.route("/employee/<id>")
 def emp_master(id):
@@ -135,7 +159,7 @@ def emp_master(id):
         return redirect(url_for('login'))
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM employee e WHERE e.id = ?", (id,))
+    cur.execute("SELECT * FROM employee e WHERE e.id = ?", (id, ))
     emp = cur.fetchone()
     if not emp:
         flash(f"Error: No employee entry {id}", "error")
@@ -151,10 +175,13 @@ def emp_new():
     cur = conn.cursor()
     if request.method == "POST":
         request_valid = True
-        return_data = { "form": request.form } # エラーメッセージなどを渡すためレンダラに渡すデータが大きくなる。よって辞書として管理する。
+        return_data = {
+            "form": request.form
+        }  # エラーメッセージなどを渡すためレンダラに渡すデータが大きくなる。よって辞書として管理する。
 
         ## idを確認（数値である＆被りがない）
-        entry = cur.execute('SELECT name FROM employee e WHERE e.id = ?', (request.form['id'],)).fetchone()
+        entry = cur.execute('SELECT name FROM employee e WHERE e.id = ?',
+                            (request.form['id'], )).fetchone()
         if not request.form['id'].isdigit() or entry:
             request_valid = False
             return_data["id_invalid"] = "IDが不正です。"
@@ -162,8 +189,10 @@ def emp_new():
             id = int(request.form['id'])
 
         ## managerを確認
-        manager_entry = cur.execute('SELECT name FROM employee e WHERE e.id = ?', (request.form["manager"],)).fetchone()
-        if  request.form["manager"] != "0" and not manager_entry:
+        manager_entry = cur.execute(
+            'SELECT name FROM employee e WHERE e.id = ?',
+            (request.form["manager"], )).fetchone()
+        if request.form["manager"] != "0" and not manager_entry:
             request_valid = False
             return_data["manager_invalid"] = "Manager IDが不正です。"
 
@@ -181,7 +210,8 @@ def emp_new():
             return_data["pict_invalid"] = "画像が添付されていないか、拡張子が不正です。"
         else:
             file = request.files["pict"]
-            if file.filename == "" or not file.filename.endswith(('jpg', 'jpeg', 'png')):
+            if file.filename == "" or not file.filename.endswith(
+                ('jpg', 'jpeg', 'png')):
                 request_valid = False
                 return_data["pict_invalid"] = "画像が添付されていないか、拡張子が不正です。"
             elif request_valid:
@@ -192,20 +222,25 @@ def emp_new():
         ## Insertを行う
         if request_valid:
             try:
-                cur.execute('INSERT INTO employee (id,name,salary,manager,byear,syear,pict) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                        [id, request.form['name'], 100, request.form['manager'],
-                        request.form['byear'], datetime.date.today().year, filename])
+                cur.execute(
+                    'INSERT INTO employee (id,name,salary,manager,byear,syear,pict) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                    [
+                        id, request.form['name'], 100, request.form['manager'],
+                        request.form['byear'],
+                        datetime.date.today().year, filename
+                    ])
             except sqlite3.Error as e:
                 print('sqlite3.Error occurred:', e.args[0])
                 request_valid = False
                 return_data['db_error'] = True
-            conn.commit() ## 更新はcommitが必要
+            conn.commit()  ## 更新はcommitが必要
 
         if request_valid:
-            flash("Registered. ID:{} NAME:{}（Manager:{}）".format(id, request.form['name'], manager_entry['name']))
+            flash("Registered. ID:{} NAME:{}（Manager:{}）".format(
+                id, request.form['name'], manager_entry['name']))
             return redirect(url_for("employee"))
         else:
-            return render_template("emp_new.html" ,**return_data)
+            return render_template("emp_new.html", **return_data)
 
     else:
         return render_template("emp_new.html", form={})
@@ -217,7 +252,8 @@ def emp_edit(id):
         return redirect(url_for('login'))
     conn = get_db()
     cur = conn.cursor()
-    emp = cur.execute('SELECT * FROM employee e WHERE e.id = ?',[id]).fetchone()
+    emp = cur.execute('SELECT * FROM employee e WHERE e.id = ?',
+                      [id]).fetchone()
     if not emp:
         flash(f"Invalid id {id}", "error")
         return redirect(url_for("employee"))
@@ -227,10 +263,12 @@ def emp_edit(id):
 
     if request.method == "POST":
         request_valid = True
-        return_data = { "emp": {**emp, **request.form} }
+        return_data = {"emp": {**emp, **request.form}}
 
         ## managerを確認
-        manager_entry =cur.execute('SELECT name FROM employee e WHERE e.id = ?',[request.form["manager"]]).fetchone() 
+        manager_entry = cur.execute(
+            'SELECT name FROM employee e WHERE e.id = ?',
+            [request.form["manager"]]).fetchone()
         if request.form["manager"] != "0" and not manager_entry:
             request_valid = False
             return_data["manager_invalid"] = "Manager IDが不正です。"
@@ -249,13 +287,17 @@ def emp_edit(id):
         ## Insertを行う
         if request_valid:
             try:
-                cur.execute('UPDATE employee SET  name=?, salary=?, manager=? ,pict=? WHERE id=?',
-                        [request.form['name'], request.form['salary'], request.form['manager'],filename,id])
+                cur.execute(
+                    'UPDATE employee SET  name=?, salary=?, manager=? ,pict=? WHERE id=?',
+                    [
+                        request.form['name'], request.form['salary'],
+                        request.form['manager'], filename, id
+                    ])
             except sqlite3.Error as e:
                 print('sqlite3.Error occurred:', e.args[0])
                 request_valid = False
                 return_data['db_error'] = True
-            conn.commit() ## 更新はcommitが必要
+            conn.commit()  ## 更新はcommitが必要
 
         if request_valid:
             flash(f"Updated {emp['name']} (Id:{id})")
@@ -276,12 +318,15 @@ def emp_delete(id):
     request_valid = True
 
     ## マネージャかどうか確認
-    is_manager = cur.execute('SELECT e.name FROM employee e WHERE e.manager = ?', (id,)).fetchone()
+    is_manager = cur.execute(
+        'SELECT e.name FROM employee e WHERE e.manager = ?',
+        (id, )).fetchone()
     if not is_manager:
-        emp = cur.execute('SELECT name FROM employee e WHERE e.id = ?',(id,)).fetchone()
+        emp = cur.execute('SELECT name FROM employee e WHERE e.id = ?',
+                          (id, )).fetchone()
         ## Deleteを行う
         try:
-            cur.execute('DELETE FROM employee WHERE id = ?',(id,))
+            cur.execute('DELETE FROM employee WHERE id = ?', (id, ))
         except sqlite3.Error as e:
             print('sqlite3.Error occurred:', e.args[0])
             request_valid = False
@@ -294,6 +339,7 @@ def emp_delete(id):
     else:
         flash('Managerとして登録されています', "error")
         return redirect(url_for('emp_master', id=id))
+
 
 # pythonを直接実行したときでもflask run --debugと同じ挙動となるようにする
 if __name__ == '__main__':
