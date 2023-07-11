4dadc4d140842c4aefd3d738f3861c84a7085f05
Oyaki122 <oc.yaki122@gmail.com>
2023-07-11 15:48:57 +0900

    api追加

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import hashlib
 import sqlite3
 import os
@@ -49,7 +50,8 @@ def get_cars():
     conn = get_db()
     cur = conn.cursor()
     cars = cur.execute('SELECT * FROM cars').fetchall()
-    return json.dumps({'cars': [dict(car) for car in cars]})
+    return json.dumps({'cars': [dict(car) for car in cars]},
+                      ensure_ascii=False)
 
 
 @app.route('/api/get_car/<car_id>')
@@ -58,7 +60,7 @@ def get_car(car_id):
     cur = conn.cursor()
     car = cur.execute('SELECT * FROM cars WHERE car_id = ?',
                       (car_id, )).fetchone()
-    return json.dumps({'car': dict(car)})
+    return json.dumps({'car': dict(car)}, ensure_ascii=False)
 
 
 @app.route('/api/get_stations')
@@ -66,7 +68,8 @@ def get_stations():
     conn = get_db()
     cur = conn.cursor()
     stations = cur.execute('SELECT * FROM stations').fetchall()
-    return json.dumps({'stations': [dict(station) for station in stations]})
+    return json.dumps({'stations': [dict(station) for station in stations]},
+                      ensure_ascii=False)
 
 
 @app.route('/api/get_station/<station_id>')
@@ -75,7 +78,8 @@ def get_station(station_id):
     cur = conn.cursor()
     station = cur.execute('SELECT * FROM stations WHERE station_id = ?',
                           (station_id, )).fetchone()
-    return json.dumps({'station': dict(station) if station else None})
+    return json.dumps({'station': dict(station) if station else None},
+                      ensure_ascii=False)
 
 
 @app.route('/api/get_drivers')
@@ -83,7 +87,8 @@ def get_drivers():
     conn = get_db()
     cur = conn.cursor()
     drivers = cur.execute('SELECT * FROM drivers').fetchall()
-    return json.dumps({'drivers': [dict(driver) for driver in drivers]})
+    return json.dumps({'drivers': [dict(driver) for driver in drivers]},
+                      ensure_ascii=False)
 
 
 @app.route('/api/get_driver/<driver_id>')
@@ -92,7 +97,7 @@ def get_driver(driver_id):
     cur = conn.cursor()
     driver = cur.execute('SELECT * FROM drivers WHERE driver_id = ?',
                          (driver_id, )).fetchone()
-    return json.dumps({'driver': dict(driver)})
+    return json.dumps({'driver': dict(driver)}, ensure_ascii=False)
 
 
 @app.route('/api/get_conductors')
@@ -100,7 +105,8 @@ def get_crews():
     conn = get_db()
     cur = conn.cursor()
     crews = cur.execute('SELECT * FROM conductors').fetchall()
-    return json.dumps({'crews': [dict(crew) for crew in crews]})
+    return json.dumps({'crews': [dict(crew) for crew in crews]},
+                      ensure_ascii=False)
 
 
 @app.route('/api/get_conductor/<crew_id>')
@@ -109,7 +115,7 @@ def get_crew(crew_id):
     cur = conn.cursor()
     crew = cur.execute('SELECT * FROM conductors WHERE conductor_id = ?',
                        (crew_id, )).fetchone()
-    return json.dumps({'crew': dict(crew)})
+    return json.dumps({'crew': dict(crew)}, ensure_ascii=False)
 
 
 @app.route('/api/get_schedule_by_station/<station_id>')
@@ -129,7 +135,8 @@ def get_schedule_by_station(station_id):
           order by stops.departure_time
                             """, (station_id, )).fetchall()
     return json.dumps(
-        {'schedules': [dict(schedule) for schedule in schedules]})
+        {'schedules': [dict(schedule) for schedule in schedules]},
+        ensure_ascii=False)
 
 
 @app.route('/api/get_car_schedule/<car_id>')
@@ -148,7 +155,8 @@ def get_car_schedule(car_id):
         order by crewOnBoard.train_id, stops.departure_time NULLS LAST, stops.arrival_time  
   """, (car_id, )).fetchall()
     return json.dumps(
-        {'schedules': [dict(schedule) for schedule in schedules]})
+        {'schedules': [dict(schedule) for schedule in schedules]},
+        ensure_ascii=False)
 
 
 @app.route('/api/get_driver_schedule/<driver_id>')
@@ -156,61 +164,233 @@ def get_driver_schedule(driver_id):
     conn = get_db()
     cur = conn.cursor()
     schedules = cur.execute(
-        """
-        select crewOnBoard.train_id, stations.name, stops.arrival_time, stops.departure_time
-        from stops, stations, crewOnBoard, train
-        where crewOnBoard.train_id = stops.train_id
-          and crewOnBoard.station_id = stops.station_id
-          and crewOnBoard.driver_id = ?
-          and train.train_id = crewOnBoard.train_id
-          and stations.station_id = crewOnBoard.station_id
+        """       
+        select * from driver_schedule where driver_id = ?
         """, (driver_id, )).fetchall()
     return json.dumps(
-        {'schedules': [dict(schedule) for schedule in schedules]})
+        {'schedules': [dict(schedule) for schedule in schedules]},
+        ensure_ascii=False)
 
 
-@app.route('/api/set_crew_on_board', methods=['POST'])
-def set_crew_on_board():
+@app.route('/api/get_train_schedule/<train_id>')
+def get_train_schedule(train_id):
     conn = get_db()
     cur = conn.cursor()
-    crew_on_board = request.get_json()
-    cur.execute(
-        """
-        insert into crewOnBoard (train_id, station_id, driver_id, conductor_id)
-        values (?, ?, ?, ?)
-        """, (crew_on_board['train_id'], crew_on_board['station_id'],
-              crew_on_board['driver_id'], crew_on_board['conductor_id']))
+    schedules = cur.execute(
+        """       
+        select * from stops where train_id = ?
+        order by stops.departure_time NULLS LAST, stops.arrival_time  
+        """, (train_id, )).fetchall()
+    return json.dumps({schedules: [dict(schedule) for schedule in schedules]})
+
+
+# stops
+@app.route('/api/stops/<train_id>/edit', methods=['POST'])
+def update_stops(train_id):
+    conn = get_db()
+    cur = conn.cursor()
+    stops = request.get_json()["record"]
+    for i in stops:
+        station_id = i['station_id']
+        arrival_time = i['arrival_time']
+        departure_time = i['departure_time']
+
+        recode = cur.execute(
+            """
+            select * from stops where train_id = ? and station_id = ? 
+            """, (train_id, station_id)).fetchall()
+        if len(recode) == 0:
+            cur.execute(
+                """
+                insert into stops (train_id, station_id, arrival_time, departure_time)
+                values (?, ?, ?, ?)
+                """, (train_id, station_id, arrival_time, departure_time))
+        else:
+            cur.execute(
+                """
+                update stops
+                set arrival_time = ?,
+                    departure_time = ?
+                where train_id = ?
+                  and station_id = ?
+                """, (arrival_time, departure_time, train_id, station_id))
     conn.commit()
     return json.dumps({'result': True})
 
 
-@app.route('/api/set_trains', methods=['POST'])
-def set_trains():
+@app.route('/api/stops/<train_id>/delete', methods=['POST'])
+def delete_stops(train_id, station_id):
+    conn = get_db()
+    cur = conn.cursor()
+    target = request.get_json()["target"]
+    for i in target:
+        cur.execute(
+            """
+            delete from stops
+            where trainget_driver_schedule/1_id = ?
+              and station_id = ?
+            """, (train_id, i))
+    conn.commit()
+    return json.dumps({'result': True})
+
+
+# crewrange
+@app.route('/api/crewRange/<train_id>')
+def get_crewRange(train_id):
+    conn = get_db()
+    cur = conn.cursor()
+    crews = cur.execute(
+        """
+        select * from crewOnBoard where train_id = ?
+        """, (train_id, )).fetchall()
+    return json.dumps({'range': [dict(crew) for crew in crews]},
+                      ensure_ascii=False)
+
+
+@app.route('/api/crewRange/<train_id>/edit', methods=['POST'])
+def update_crewRange(train_id, station_id):
+    conn = get_db()
+    cur = conn.cursor()
+    data = request.get_json()["data"]
+
+    for crews in data:
+        recode = cur.execute(
+            """
+            select * from crewOnBoard where train_id = ? and station_id = ?
+            """, (train_id, station_id)).fetchall()
+        if len(recode) == 0:
+            cur.execute(
+                """
+                insert into crewOnBoard (train_id, station_id, driver_id, conductor_id)
+                values (?, ?, ?, ?)
+                """, (train_id, station_id, crews['driver_id'],
+                      crews['conductor_id']))
+        else:
+            cur.execute(
+                """
+                update crewOnBoard
+                set driver_id = ?,
+                    conductor_id = ?
+                where train_id = ?
+                  and station_id = ?
+                """, (crews['driver_id'], crews['conductor_id'], train_id,
+                      station_id))
+    conn.commit()
+    return json.dumps({'result': True})
+
+
+@app.route('/api/crewRange/<train_id>/delete', methods=['POST'])
+def delete_crewRange(train_id):
+    conn = get_db()
+    cur = conn.cursor()
+    data = request.get_json()["target"]
+    for i in data:
+        cur.execute(
+            """
+            delete from crewOnBoard
+            where train_id = ?
+              and station_id = ?
+            """, (train_id, i))
+    conn.commit()
+    return json.dumps({'result': True})
+
+
+# train
+@app.route('/api/train_details')
+def get_trains():
+    conn = get_db()
+    cur = conn.cursor()
+    trains = cur.execute('SELECT * FROM train').fetchall()
+    return json.dumps({'trains': [dict(train) for train in trains]},
+                      ensure_ascii=False)
+
+
+@app.route('/api/train_detail/<train_id>', methods=['GET'])
+def get_train(train_id):
+    conn = get_db()
+    cur = conn.cursor()
+    train = cur.execute(
+        """
+        select * from train where train_id = ?
+        """, (train_id, )).fetchone()
+    return json.dumps({'train': dict(train)}, ensure_ascii=False)
+
+
+@app.route('/api/train_detail/<train_id>/update', methods=['POST'])
+def update_train(train_id):
     conn = get_db()
     cur = conn.cursor()
     train = request.get_json()
+
     cur.execute(
         """
-        insert into train (train_id, car_id, origin_id, destinaion_id)
-        values (?, ?, ?)
-        """, (train['train_id'], train['car_id'], train['origin_id'],
-              train['destination_id']))
+        update train
+        set destination = ?,
+            origin = ?,
+            car = ?
+        where train_id = ?
+        """, (train['destination'], train['origin'], train['car'], train_id))
     conn.commit()
     return json.dumps({'result': True})
 
 
-@app.route('/api/set_stops', methods=['POST'])
-def set_stops():
+@app.route('/api/train/<train_id>/delete', methods=['POST'])
+def delete_train(train_id):
     conn = get_db()
     cur = conn.cursor()
-    stop = request.get_json()
     cur.execute(
         """
-        insert into stops (train_id, station_id, arrival_time, departure_time)
-        values (?, ?, ?, ?)
-        """, (stop['train_id'], stop['station_id'], stop['arrival_time'],
-              stop['departure_time']))
+        delete from train
+        where train_id = ?
+        """, (train_id, ))
+
+    cur.execute(
+        """
+        delete from stops
+        where train_id = ?
+        """, (train_id, ))
+
+    cur.execute(
+        """
+        delete from crewOnBoard
+        where train_id = ?
+        """, (train_id, ))
+
     conn.commit()
+    return json.dumps({'result': True})
+
+
+# train全般操作
+@app.route('/api/add_train', methods=['POST'])
+def set_train_schedule():
+    conn = get_db()
+    cur = conn.cursor()
+    train_schedule = request.get_json()["train"]
+    for i in train_schedule["schedule"]:
+        cur.execute(
+            """
+            insert into stops (train_id, station_id, arrival_time, departure_time)
+            values (?, ?, ?, ?)
+            """, (i['train_id'], i['station_id'], i['arrival_time'],
+                  i['departure_time']))
+
+        cur.execute(
+            """
+            insert into crewOnBoard (train_id, station_id, driver_id, conductor_id)
+            values (?, ?, ?, ?)
+            """, (i['train_id'], i['station_id'], i['driver_id'],
+                  i['conductor_id']))
+
+    train = train_schedule["detail"]
+    cur.execute(
+        """
+        insert into train (train_id, car, origin, destinaion)
+        values (?, ?, ?, ?)
+        """, (train['train_id'], train['car_id'], train['origin_id'],
+              train['destination_id']))
+
+    conn.commit()
+
     return json.dumps({'result': True})
 
 
